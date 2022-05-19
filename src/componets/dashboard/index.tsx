e97a706d26c7c23348d22db2a355dfee7f19df2d
desshub <desshub95@gmail.com>
2022-05-19 22:05:55 +0300

    Set up redux toolkit

--- src/componets/dashboard/index.tsx
+++ src/componets/dashboard/index.tsx
@@ -1,30 +1,43 @@
-import React from "react";
+import React, {useEffect} from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./index.scss";
 import { Search } from "react-bootstrap-icons";
 import MarketLeaders from "../MarketLeaders";
 import CoinsCard from "../CoinsCard";
 import EventsCard from "../EventsCard";
+import { fetchCoins } from "../../features/coinsSlice";
+import { AppDispatch, RootState } from "../../app/store";
 
 
-export default function Dashboard () {
+export default function Dashboard() {
+  // initialize the redux hook
+  const dispatch = useDispatch<AppDispatch>();
+  const { coins, isLoading, hasErrors } = useSelector(
+    (state: RootState) => state.coins
+  );
+  console.log("dashboard", coins, isLoading, hasErrors);
 
-    return (
-      <div className="dashboard">
-        <div className="search-container">
-          <Search className="search-icon" />
-          <input className="search-input" placeholder="search" />
+  useEffect(() => {
+    dispatch(fetchCoins());
+  }, [dispatch]);
+
+  return (
+    <div className="dashboard">
+      <div className="search-container">
+        <Search className="search-icon" />
+        <input className="search-input" placeholder="search" />
+      </div>
+
+      <div className="content-section">
+        <div className="left-col">
+          <MarketLeaders />
+
+          <CoinsCard />
         </div>
-
-        <div className="content-section">
-          <div className="left-col">
-            <MarketLeaders />
-
-            <CoinsCard />
-          </div>
-          <div className="right-col">
-            <EventsCard />
-          </div>
+        <div className="right-col">
+          <EventsCard />
         </div>
       </div>
-    );
+    </div>
+  );
 }
---
+++ src/features/coinsSlice.ts
@@ -0,0 +1,44 @@
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+import ApiService from "../services/ApiService";
+
+export const fetchCoins = createAsyncThunk("coins/coinsList", () =>
+  ApiService.get(`/coins/list`)
+    .then((response) => response.data)
+    .catch((error) => error)
+);
+
+interface CoinsState {
+    coins: any[];
+    isLoading: boolean;
+    hasErrors: boolean;
+}
+
+const initialState: CoinsState = {
+  coins: [],
+  isLoading: false,
+  hasErrors: false
+};
+
+export const coinsSlice = createSlice({
+  name: "coins",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [fetchCoins.pending.type]: (state, action) => {
+      state.isLoading = true;
+    },
+    [fetchCoins.fulfilled.type]: (state, action) => {
+      state.coins = action.payload;
+      state.isLoading = false;
+      state.hasErrors = false;
+    },
+    [fetchCoins.rejected.type]: (state, action) => {
+      state.isLoading = false;
+      state.hasErrors = true;
+    },
+  },
+});
+
+// export const {} = coinsSlice.actions;
+
+export default coinsSlice.reducer;
